75c97bbf68617e4751de8b7c2abb6f146975486a
nastyaalmaeva <nastyaalmaeva@mail.ru>
2025-03-23 22:46:06 +0300

    Added abstract Property class

---
+++ inheritance_and_polymorphism/properties/src/properties/Property.java
@@ -0,0 +1,53 @@
+package properties;
+
+import java.util.Objects;
+
+public abstract class Property {
+    private double worth;
+
+    public Property(double worth) {
+        setWorth(worth);
+    }
+
+    public void sell() {
+        System.out.println("The property has been sold.");
+    }
+
+    public void transfer() {
+        System.out.println("The property has been transferred.");
+    }
+
+    public abstract Object calculateTax();
+
+    public double getWorth() {
+        return worth;
+    }
+
+    protected void setWorth(double worth) {
+        if (worth > 0) {
+            this.worth = worth;
+        }
+        else {
+            throw new IllegalArgumentException("Worth cannot be less than zero.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Property property = (Property) o;
+        return Double.compare(worth, property.worth) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(worth);
+    }
+
+    @Override
+    public String toString() {
+        return  "Class: " + this.getClass().getSimpleName() + "\n" +
+                "Worth: " + getWorth();
+    }
+}
